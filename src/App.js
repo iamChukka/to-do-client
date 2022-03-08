5da89d00a68cbc5e85e37b7e22723680aa421fc2
Chukwuka Eluchie <eluchiechukwuka@yahoo.co.uk>
2022-03-08 22:36:50 +0100

    First React commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import Home from "./components/home.component";
+import Register from "./components/register.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <p>ypxs</p>
+
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={Home} />
+        <Route path="/register" component={Register} />
+        {/* 
+      <Route path="/" exact component={}/>
+      <Route path="/" exact component={}/>
+ */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.component.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" href="/">
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" href="/">
+                Home <span class="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/register">
+                Register
+              </Link>
+            </li>
+            {/* <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li> */}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
